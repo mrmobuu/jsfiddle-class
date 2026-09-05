d417dd568a7b43b85427f0e85a796af89bba84b3
mrmobuu <adamyabansal@gmail.com>
2026-09-06 02:19:52 +0530

    dist removal

--- backend/dist/generated/prisma/enums.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const SubmissionStatus: {
-    readonly Processing: "Processing";
-    readonly Suceess: "Suceess";
-    readonly Failure: "Failure";
-};
-export type SubmissionStatus = (typeof SubmissionStatus)[keyof typeof SubmissionStatus];
-//# sourceMappingURL=enums.d.ts.map
--- backend/dist/generated/prisma/models.d.ts
+++
@@ -1,3 +0,0 @@
-export type * from './models/Submission.js';
-export type * from './commonInputTypes.js';
-//# sourceMappingURL=models.d.ts.map
